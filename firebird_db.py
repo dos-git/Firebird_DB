fc90d4666af0fcc3a6e8d7303dd6351b3da37ce4
DominikStefanowicz <dominik.stefanowicz@gmail.com>
2015-10-11 23:48:16 +0200

    Some evaluation about retrieving db structure. Add commit request when table is created - importan because without it, table will not be stored in db.

--- firebird_db.py
+++ firebird_db.py
@@ -18,8 +18,11 @@ fb_path = UT_DIR + os.sep + FB_DIR + os.sep + DB_NAME
 full_path = os.getcwd() + os.sep + fb_path
 
 sql_select_all = "select * from CLIENTS"
+# FIELD LENGHT makes error -maybe because there is no entries in db
+#sql_select_struct = "SELECT RDB$FIELD_NAME, RDB$DESCRIPTION, RDB$DEFAULT_VALUE, RDB$NULL_FLAG, RDB$FIELD_LENGTH FROM RDB$RELATION_FIELDS WHERE RDB$RELATION_NAME='CLIENTS';"
+sql_select_struct = "SELECT RDB$FIELD_NAME, RDB$DESCRIPTION, RDB$DEFAULT_VALUE, RDB$NULL_FLAG FROM RDB$RELATION_FIELDS WHERE RDB$RELATION_NAME='CLIENTS';"
 sql_create_db = "create database '%s' user 'sysdba' password 'masterkey'" % fb_path
-sql_create_table_1 = "create table CLIENTS (ID_client INTEGER NOT NULL, COMPANY VARCHAR(50), NAME VARCHAR(50))"
+sql_create_table_1 = "create table CLIENTS (ID_client INTEGER NOT NULL, COMPANY VARCHAR(20), NAME VARCHAR(20))"
 sql_create_table_2 = "create table ITEMS (ID_item INTEGER NOT NULL, NAME VARCHAR(50), PRIZE INTEGER NOT NULL, CREATE_TIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
 
 # full_path is not seen because script is run as dos user - only connection to db is done as firebird user
@@ -27,16 +30,17 @@ sql_create_table_2 = "create table ITEMS (ID_item INTEGER NOT NULL, NAME VARCHAR
 con = None
 cur = None
 try:
-    #con = fdb.create_database(sql_create_db)
-    con = fdb.connect(dsn=fb_path, user='sysdba', password='masterkey')
+    con = fdb.create_database(sql_create_db)
+    #con = fdb.connect(dsn=fb_path, user='sysdba', password='masterkey')
     cur = con.cursor()
-    #cur.execute(sql_create_table_1)
+    cur.execute(sql_create_table_1)
+    con.commit()    # commit is a part of table creation, without it, table will not be created
     #cur.execute(sql_create_table_2)
-    data = cur.execute(sql_select_all)
+    #con.commit()    # commit is a part of table creation, without it, table will not be created
+
+    data = cur.execute(sql_select_struct)
     print data.fetchall()
-    #con.commit()
-    cur.close()
-    con.close()
+
 except fdb.DatabaseError as e:
     print "[0] %s" % str(e.args[0])
     print "[1] %s" % str(e.args[1])
